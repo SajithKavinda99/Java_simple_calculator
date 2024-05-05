a0af849fa30a102f0d744e0d34db683be038be16
SajithKavinda99 <sajithbusiness094@gmail.com>
2024-05-05 12:40:21 +0530

    Initial commit

---
+++ src/java_simple_calculator/Java_simple_calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_simple_calculator;
+
+/**
+ *
+ * @author dell
+ */
+public class Java_simple_calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        calculator start = new calculator();
+        start.setVisible(true);
+    }
+    
+}
